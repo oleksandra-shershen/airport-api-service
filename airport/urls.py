44618fc6872096a53504abe4fe95cb9aec956104
Shershen Alexandra <hornet240204@gmail.com>
2024-06-06 15:50:15 +0300

    Add url for AirplaneType

--- airport/urls.py
+++ airport/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from airport.views import AirportViewSet, AirplaneViewSet, CrewViewSet
+from airport.views import AirportViewSet, AirplaneViewSet, AirplaneTypeViewSet, CrewViewSet
 
 router = routers.DefaultRouter()
 router.register("airports", AirportViewSet)
 router.register("airplanes", AirplaneViewSet)
+router.register("airplane_types", AirplaneTypeViewSet)
 router.register("crews", CrewViewSet)
 
 urlpatterns = [path("", include(router.urls))]
