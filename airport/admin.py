0042c476175b06e89d8607993cd1f3d7d0e7df42
Shershen Alexandra <hornet240204@gmail.com>
2024-06-06 14:43:35 +0300

    Add models to the airport app, urls app, admin

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from airport.models import (
+    Airport,
+    Airplane,
+    AirplaneType,
+    Route,
+    Ticket,
+    Order,
+    Flight,
+    Crew,
+
+)
+
+admin.site.register(Airport)
+admin.site.register(AirplaneType)
+admin.site.register(Route)
+admin.site.register(Order)
+admin.site.register(Ticket)
+admin.site.register(Flight)
+admin.site.register(Crew)
+admin.site.register(Airplane)
+
