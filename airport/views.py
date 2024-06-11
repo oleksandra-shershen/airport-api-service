ae9ff84e0e4ddd68bdc1d775658a4d0c6919ca64
Shershen Alexandra <hornet240204@gmail.com>
2024-06-11 18:10:21 +0300

    Add filtering

--- airport/views.py
+++ airport/views.py
@@ -1,8 +1,7 @@
 from datetime import datetime
-
 from django.db.models import F, Count
 from drf_spectacular.types import OpenApiTypes
-from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiResponse
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets, mixins, status
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
@@ -10,7 +9,7 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from airport.models import Airport, AirplaneType, Airplane, Crew, Route, Flight, Order
+from airport.models import Airport, Route, AirplaneType, Airplane, Crew, Flight, Order
 from airport.permissions import IsAdminOrIfAuthenticatedReadOnly
 from airport.serializers import (
     AirportSerializer,
@@ -30,6 +29,7 @@ from airport.serializers import (
     AirplaneImageSerializer,
 )
 
+
 class AirportViewSet(
     mixins.CreateModelMixin,
     mixins.ListModelMixin,
@@ -38,17 +38,6 @@ class AirportViewSet(
     queryset = Airport.objects.all()
     serializer_class = AirportSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
-
-    @extend_schema(
-        summary="Retrieve a list of airports",
-        responses={
-            200: OpenApiResponse(response=AirportSerializer, description="Successful response with a list of airports"),
-            400: OpenApiResponse(description="Bad Request - Invalid query parameters"),
-            401: OpenApiResponse(description="Unauthorized - Authentication credentials were not provided or are invalid"),
-        }
-    )
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
 
 
 class AirplaneTypeViewSet(
@@ -60,17 +49,6 @@ class AirplaneTypeViewSet(
     serializer_class = AirplaneTypeSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
-    @extend_schema(
-        summary="Retrieve a list of airplane types",
-        responses={
-            200: OpenApiResponse(response=AirplaneTypeSerializer, description="Successful response with a list of airplane types"),
-            400: OpenApiResponse(description="Bad Request - Invalid query parameters"),
-            401: OpenApiResponse(description="Unauthorized - Authentication credentials were not provided or are invalid"),
-        }
-    )
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
-
 
 class CrewViewSet(
     mixins.CreateModelMixin,
@@ -80,17 +58,6 @@ class CrewViewSet(
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
-
-    @extend_schema(
-        summary="Retrieve a list of crew members",
-        responses={
-            200: OpenApiResponse(response=CrewSerializer, description="Successful response with a list of crew members"),
-            400: OpenApiResponse(description="Bad Request - Invalid query parameters"),
-            401: OpenApiResponse(description="Unauthorized - Authentication credentials were not provided or are invalid"),
-        }
-    )
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
 
 
 class AirplaneViewSet(viewsets.ModelViewSet):
@@ -103,24 +70,27 @@ class AirplaneViewSet(viewsets.ModelViewSet):
             return AirplaneListSerializer
         elif self.action == "retrieve":
             return AirplaneDetailSerializer
-        elif self.action == "upload-image":
+        elif self.action == "upload_image":
             return AirplaneImageSerializer
         return AirplaneSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        airplane_type = self.request.query_params.get("airplane_type")
+        name = self.request.query_params.get("name")
+
+        if airplane_type:
+            queryset = queryset.filter(airplane_type__id=airplane_type)
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset
 
     @action(
         methods=["POST"],
         detail=True,
         url_path="upload-image",
         permission_classes=[IsAdminUser],
-    )
-    @extend_schema(
-        summary="Upload an image for a specific airplane",
-        responses={
-            200: OpenApiResponse(response=AirplaneImageSerializer, description="Image uploaded successfully"),
-            400: OpenApiResponse(description="Bad Request - Invalid image data"),
-            401: OpenApiResponse(description="Unauthorized - Authentication credentials were not provided or are invalid"),
-            403: OpenApiResponse(description="Forbidden - You do not have permission to perform this action"),
-        }
     )
     def upload_image(self, request, pk=None):
         """Endpoint for uploading image to specific airplane"""
@@ -134,12 +104,18 @@ class AirplaneViewSet(viewsets.ModelViewSet):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @extend_schema(
-        summary="Retrieve a list of airplanes",
-        responses={
-            200: OpenApiResponse(response=AirplaneListSerializer, description="Successful response with a list of airplanes"),
-            400: OpenApiResponse(description="Bad Request - Invalid query parameters"),
-            401: OpenApiResponse(description="Unauthorized - Authentication credentials were not provided or are invalid"),
-        }
+        parameters=[
+            OpenApiParameter(
+                "airplane_type",
+                type=OpenApiTypes.INT,
+                description="Filter by airplane type id (ex. ?airplane_type=2)",
+            ),
+            OpenApiParameter(
+                "name",
+                type=OpenApiTypes.STR,
+                description="Filter by airplane name (ex. ?name=boeing)",
+            ),
+        ]
     )
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
@@ -157,17 +133,6 @@ class RouteViewSet(viewsets.ModelViewSet):
             return RouteDetailSerializer
         return RouteSerializer
 
-    @extend_schema(
-        summary="Retrieve a list of routes",
-        responses={
-            200: OpenApiResponse(response=RouteListSerializer, description="Successful response with a list of routes"),
-            400: OpenApiResponse(description="Bad Request - Invalid query parameters"),
-            401: OpenApiResponse(description="Unauthorized - Authentication credentials were not provided or are invalid"),
-        }
-    )
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
-
 
 class FlightViewSet(viewsets.ModelViewSet):
     queryset = (
@@ -176,8 +141,8 @@ class FlightViewSet(viewsets.ModelViewSet):
         .prefetch_related("crews")
         .annotate(
             tickets_available=(
-                F("airplane__rows") * F("airplane__seats_in_row")
-                - Count("tickets")
+                    F("airplane__rows") * F("airplane__seats_in_row")
+                    - Count("tickets")
             )
         )
     )
@@ -191,13 +156,40 @@ class FlightViewSet(viewsets.ModelViewSet):
             return FlightDetailSerializer
         return FlightSerializer
 
+    def get_queryset(self):
+        queryset = self.queryset
+        route = self.request.query_params.get("route")
+        airplane = self.request.query_params.get("airplane")
+        departure_time = self.request.query_params.get("departure_time")
+
+        if route:
+            queryset = queryset.filter(route__id=route)
+        if airplane:
+            queryset = queryset.filter(airplane__id=airplane)
+        if departure_time:
+            departure_datetime = datetime.strptime(departure_time, "%Y-%m-%d")
+            queryset = queryset.filter(departure_time__date=departure_datetime.date())
+
+        return queryset
+
     @extend_schema(
-        summary="Retrieve a list of flights",
-        responses={
-            200: OpenApiResponse(response=FlightListSerializer, description="Successful response with a list of flights"),
-            400: OpenApiResponse(description="Bad Request - Invalid query parameters"),
-            401: OpenApiResponse(description="Unauthorized - Authentication credentials were not provided or are invalid"),
-        }
+        parameters=[
+            OpenApiParameter(
+                "route",
+                type=OpenApiTypes.INT,
+                description="Filter by route id (ex. ?route=2)",
+            ),
+            OpenApiParameter(
+                "airplane",
+                type=OpenApiTypes.INT,
+                description="Filter by airplane id (ex. ?airplane=2)",
+            ),
+            OpenApiParameter(
+                "departure_time",
+                type=OpenApiTypes.DATE,
+                description="Filter by departure date (ex. ?departure_time=2024-06-11)",
+            ),
+        ]
     )
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
@@ -211,7 +203,7 @@ class OrderPagination(PageNumberPagination):
 class OrderViewSet(
     mixins.ListModelMixin,
     mixins.CreateModelMixin,
-    viewsets.GenericViewSet,
+    GenericViewSet,
 ):
     queryset = Order.objects.prefetch_related(
         "tickets__flight__route", "tickets__flight__airplane"
@@ -231,14 +223,3 @@ class OrderViewSet(
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
-    @extend_schema(
-        summary="Retrieve a list of orders",
-        responses={
-            200: OpenApiResponse(response=OrderListSerializer, description="Successful response with a list of orders"),
-            400: OpenApiResponse(description="Bad Request - Invalid query parameters"),
-            401: OpenApiResponse(description="Unauthorized - Authentication credentials were not provided or are invalid"),
-        }
-    )
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
