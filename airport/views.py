ccfc31e4aa6a5ab587235270704dd476c9101ff7
Shershen Alexandra <hornet240204@gmail.com>
2024-06-11 15:39:42 +0300

    Add custom endpoint

--- airport/views.py
+++ airport/views.py
@@ -1,7 +1,9 @@
 from django.db.models import F, Count
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from airport.models import Airport, AirplaneType, Airplane, Crew, Route, Flight, Order
@@ -20,7 +22,7 @@ from airport.serializers import (
     FlightDetailSerializer,
     FlightSerializer,
     OrderSerializer,
-    OrderListSerializer,
+    OrderListSerializer, AirplaneImageSerializer,
 )
 
 
@@ -59,7 +61,26 @@ class AirplaneViewSet(viewsets.ModelViewSet):
             return AirplaneListSerializer
         elif self.action == "retrieve":
             return AirplaneDetailSerializer
+        elif self.action == "upload-image":
+            return AirplaneImageSerializer
         return AirplaneSerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsAdminUser],
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading image to specific airplane"""
+        airplane = self.get_object()
+        serializer = self.get_serializer(airplane, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RouteViewSet(viewsets.ModelViewSet):
