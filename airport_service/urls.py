b5ff4f87e4abe9ccef8a7081659f9d4c903f7bf2
Shershen Alexandra <hornet240204@gmail.com>
2024-06-11 19:59:40 +0300

    Add docker files. Remove permissions

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -8,6 +7,7 @@ from drf_spectacular.views import (
     SpectacularRedocView,
 )
 
+from airport_service import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
