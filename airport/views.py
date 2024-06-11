a0cc9579bcc95de69ca46aa7954dcc3cd46dbd1e
Shershen Alexandra <hornet240204@gmail.com>
2024-06-11 15:59:32 +0300

    Add filtering and spectacular integration to viewsets

--- airport/views.py
+++ airport/views.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 from django.db.models import F, Count
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets, mixins, status
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
@@ -7,7 +11,6 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from airport.models import Airport, AirplaneType, Airplane, Crew, Route, Flight, Order
-
 from airport.serializers import (
     AirportSerializer,
     AirplaneSerializer,
@@ -22,7 +25,8 @@ from airport.serializers import (
     FlightDetailSerializer,
     FlightSerializer,
     OrderSerializer,
-    OrderListSerializer, AirplaneImageSerializer,
+    OrderListSerializer,
+    AirplaneImageSerializer,
 )
 
 
@@ -82,6 +86,18 @@ class AirplaneViewSet(viewsets.ModelViewSet):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "airplane_type",
+                type=OpenApiTypes.INT,
+                description="Filter by airplane type id (ex. ?airplane_type=2)",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
 
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all().select_related("source", "destination")
@@ -93,6 +109,23 @@ class RouteViewSet(viewsets.ModelViewSet):
             return RouteDetailSerializer
         return RouteSerializer
 
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "source",
+                type=OpenApiTypes.INT,
+                description="Filter by source airport id (ex. ?source=1)",
+            ),
+            OpenApiParameter(
+                "destination",
+                type=OpenApiTypes.INT,
+                description="Filter by destination airport id (ex. ?destination=2)",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
 
 class FlightViewSet(viewsets.ModelViewSet):
     queryset = (
@@ -101,8 +134,8 @@ class FlightViewSet(viewsets.ModelViewSet):
         .prefetch_related("crews")
         .annotate(
             tickets_available=(
-                    F("airplane__rows") * F("airplane__seats_in_row")
-                    - Count("tickets")
+                F("airplane__rows") * F("airplane__seats_in_row")
+                - Count("tickets")
             )
         )
     )
@@ -113,6 +146,51 @@ class FlightViewSet(viewsets.ModelViewSet):
         elif self.action == "retrieve":
             return FlightDetailSerializer
         return FlightSerializer
+
+    def get_queryset(self):
+        """Retrieve the flights with filters"""
+        date = self.request.query_params.get("date")
+        airplane_id_str = self.request.query_params.get("airplane")
+        route_id_str = self.request.query_params.get("route")
+
+        queryset = self.queryset
+
+        if date:
+            date = datetime.strptime(date, "%Y-%m-%d").date()
+            queryset = queryset.filter(departure_time__date=date)
+
+        if airplane_id_str:
+            queryset = queryset.filter(airplane_id=int(airplane_id_str))
+
+        if route_id_str:
+            queryset = queryset.filter(route_id=int(route_id_str))
+
+        return queryset
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "airplane",
+                type=OpenApiTypes.INT,
+                description="Filter by airplane id (ex. ?airplane=2)",
+            ),
+            OpenApiParameter(
+                "route",
+                type=OpenApiTypes.INT,
+                description="Filter by route id (ex. ?route=2)",
+            ),
+            OpenApiParameter(
+                "date",
+                type=OpenApiTypes.DATE,
+                description=(
+                    "Filter by datetime of Flight "
+                    "(ex. ?date=2022-10-23)"
+                ),
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
 
 class OrderPagination(PageNumberPagination):
@@ -143,3 +221,20 @@ class OrderViewSet(
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "flight",
+                type=OpenApiTypes.INT,
+                description="Filter by flight id (ex. ?flight=1)",
+            ),
+            OpenApiParameter(
+                "date",
+                type=OpenApiTypes.DATE,
+                description="Filter by order date (ex. ?date=2023-10-23)",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
