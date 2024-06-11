0cca69b104d5c31e4cc588e03b938d49332e8f8b
Shershen Alexandra <hornet240204@gmail.com>
2024-06-11 19:17:45 +0300

    Add tests

--- airport/urls.py
+++ airport/urls.py
@@ -13,11 +13,11 @@ from airport.views import (
 
 router = routers.DefaultRouter()
 router.register("airports", AirportViewSet)
-router.register("airplanes", AirplaneViewSet)
+router.register("airplanes", AirplaneViewSet, basename="airplane")
 router.register("airplane_types", AirplaneTypeViewSet)
 router.register("crews", CrewViewSet)
 router.register("routes", RouteViewSet)
-router.register("flights", FlightViewSet)
+router.register("flights", FlightViewSet, basename="flight")
 router.register("orders", OrderViewSet)
 
 urlpatterns = [path("", include(router.urls))]
